c8bb4a02c42b95165e65a9149bd4589c33c937b2
ukataka <ukatakam@walmartlabs.com>
2015-04-22 22:02:38 -0700

    adding logging

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 let http = require('http')
 let request = require('request')
+let through = require('through')
+let fs = require('fs')
 let argv = require('yargs')
 	.default('host', '127.0.0.1')
 	.argv
 let scheme = 'http://'
 let port = argv.port || argv.host === '127.0.0.1' ? 8000 : 80
-let destinationUrl = scheme + argv.host + ':' + port
+let destinationUrl = argv.url || scheme + argv.host + ':' + port
+let outputStream = argv.stream ? fs.createWriteStream(argv.stream) : process.stdout
 
 http.createServer((req, res) => {
 	for(let header in req.headers) {
@@ -15,10 +18,21 @@ http.createServer((req, res) => {
 }).listen(8000)
 
 http.createServer((req, res) => {
+	let url = destinationUrl
+	if (req.headers['x-destination-url']) {
+		url = req.headers['x-destination-url']
+	}
 	let options = {
 		headers: req.headers,
-		url : destinationUrl		
+		url : url + req.url
 	}
+	
+	outputStream.write('\nOriginal request:\n' + JSON.stringify(req.headers))
+	through(req, outputStream, {autoDestroy: false})
+
 	let destinationResponse = req.pipe(request(options))
+
+	outputStream.write('\nResponse:\n' + JSON.stringify(destinationResponse.headers))
 	destinationResponse.pipe(res)
+	through(destinationResponse, outputStream, {autoDestroy: false})
 }).listen(8001)
